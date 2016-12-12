330d1cd9b4a22b05c8916fdc2d44442aae09bc20
yoshiki tanahara <e165753@ie.u-ryukyu.ac.jp>
2016-12-13 01:24:00 +0900

    レポート5

--- src/main/jp/ac/uryukyu/ie/e165753/Main.java
+++ src/main/jp/ac/uryukyu/ie/e165753/Main.java
@@ -3,7 +3,13 @@ package jp.ac.uryukyu.ie.e165753;
 public class Main {
 
     public static void main (String[] args){
-        String str = "null" ;
+        String str = "3．14" ;
+        try {
             System.out.println(str.length());
+        }catch(NullPointerException e) {
+            System.out.println("NullPointerExceptionが発生!");
+            e.printStackTrace();
         }
+        double value = Double.parseDouble(str);
     }
+}
